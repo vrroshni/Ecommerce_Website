f02595857aa1f9c8615fc59d8a43538c9583b03c
Roshni V R <vrroshni2001@gmail.com>
2025-01-28 23:31:01 +0530

    Delete venv_Ecommerce directory

--- venv_Ecommerce/Lib/site-packages/paypal/pro/exceptions.py
+++
@@ -1,7 +0,0 @@
-class PayPalFailure(Exception):
-    def __init__(self, msg, nvp=None):
-        super().__init__(msg)
-        self.nvp = nvp
-
-    def __str__(self):
-        return f"{self.args[0]}, nvp={repr(self.nvp)}"
